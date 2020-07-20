b710ec4ae177697c9900cec89e0434ffdc3dbbbc
ArbazSk <ArbazSk@arbazshaikh513@gmail.com>
2020-07-20 20:32:17 +0530

    Selecting Single Element

---
+++ DOM/single_selector.js
@@ -0,0 +1,20 @@
+// Selecting element
+// document.getElementById();
+// document.querySelector();
+
+const val = document.getElementById('tan');
+
+// Changing Style
+val.style.width = '500px';
+val.style.height = '300px';
+val.style.background = '#545';
+val.style.color = '#fff';
+val.style.padding = '5px';
+val.style.margin = 'auto';
+
+// Change Content
+val.textContent = 'Hello World';
+val.innerText = 'innerText';
+val.innerHTML = '<em> innerHTML</em>'
+
+
