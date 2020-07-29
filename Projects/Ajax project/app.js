820934c940bf98206470f41e83fe068bfc953ff7
ArbazSk <ArbazSk@arbazshaikh513@gmail.com>
2020-07-29 16:48:57 +0530

    small AJAX project -- Chuck norris project

---
+++ Projects/Ajax project/app.js
@@ -0,0 +1,38 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e){
+    e.preventDefault();
+
+    const num = document.querySelector('input[type="number"]').value;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${num}`, true);
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            const response = JSON.parse(this.responseText);
+            console.log(response);
+
+            let output = '';
+            if(response.type === 'success'){
+                response.value.forEach(function(joke){
+                    output += `<li> ${joke.joke} `;
+                });
+
+
+
+            }else{
+                output += '<li> Something went Wrong</li>';
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
+            
+        }
+    }
+
+    xhr.send();
+
+
+
+}
